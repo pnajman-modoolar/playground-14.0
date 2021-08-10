724b55604e6bf30e3f57318189133cae05257415
Petar Najman <petar.najman@modoolar.com>
2021-08-10 12:11:33 +0200

    [ADD] app l10n_ru_doc

---
+++ l10n_ru_doc/report/report_bill.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo
+#    Copyright (C) 2015-2018 CodUP (<http://codup.com>).
+#
+##############################################################################
+
+from odoo import api, models
+from odoo.addons.l10n_ru_doc.report_helper import QWebHelper
+
+class RuBillReport(models.AbstractModel):
+    _name = 'report.l10n_ru_doc.report_bill'
+
+    @api.multi
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['account.invoice'].browse(docids)
+        return {
+            'helper': QWebHelper(),
+            'doc_ids': docs.ids,
+            'doc_model': 'account.invoice',
+            'docs': docs
+        }
